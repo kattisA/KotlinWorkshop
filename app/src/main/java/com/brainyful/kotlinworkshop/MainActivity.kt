47d89481e1dd05b8794ef3047b29f6e8acb1b1b4
Katarina Lejonlid <katarina.lejonlid@aurorainnovation.com>
2019-04-11 16:33:49 +0200

    New icon, button with toast

--- app/src/main/java/com/brainyful/kotlinworkshop/MainActivity.kt
+++ app/src/main/java/com/brainyful/kotlinworkshop/MainActivity.kt
@@ -2,11 +2,28 @@ package com.brainyful.kotlinworkshop
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener{
+            toastMsg("Hej Bruce Willis")
+        }
+    }
+
+    fun makeToast(view: View){
+        toastMsg("Yippie Kay yay")
+    }
+    fun toastMsg(msg: String) {
+        val toast = Toast.makeText(this, msg, Toast.LENGTH_LONG)
+        toast.show()
     }
 }
