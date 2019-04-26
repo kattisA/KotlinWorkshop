359e166f7275452e0010c8c30908b4f2e41c4c1a
Katarina Lejonlid <katarina.lejonlid@aurorainnovation.com>
2019-04-26 09:06:57 +0200

    Added listview with a toast on click

--- app/src/main/java/com/brainyful/kotlinworkshop/MainActivity.kt
+++ app/src/main/java/com/brainyful/kotlinworkshop/MainActivity.kt
@@ -3,27 +3,39 @@ package com.brainyful.kotlinworkshop
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
 
+
+    var array = arrayOf("Martini", "Gin and Tonic", "Cosmopolitan", "Mohito", "Margherita", "Manhattan")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val adapter = ArrayAdapter(this,
+            R.layout.listview_item, array)
 
-        val button = findViewById<Button>(R.id.button)
-        button.setOnClickListener{
-            toastMsg("Hej Bruce Willis")
+        val listView: ListView = this.findViewById(R.id.drink_list)
+        listView.adapter = adapter
+
+        listView.onItemClickListener = object : AdapterView.OnItemClickListener {
+
+            override fun onItemClick(parent: AdapterView<*>, view: View,
+                                     position: Int, id: Long) {
+
+                // value of item that is clicked
+                val itemValue = listView.getItemAtPosition(position) as String
+
+                // Toast the values
+                toastMsg("Position :$position\nItem Value : $itemValue", applicationContext)
+
+            }
         }
-    }
 
-    fun makeToast(view: View){
-        toastMsg("Yippie Kay yay")
-    }
-    fun toastMsg(msg: String) {
-        val toast = Toast.makeText(this, msg, Toast.LENGTH_LONG)
-        toast.show()
     }
 }
